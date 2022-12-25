ae4a12e58bcf8bf4eb8cc9779fe5201ddf116c88
ragulzaheer <ragulraj03@gmail.com>
2022-12-25 16:08:22 +0530

    Ragul

---
+++ student2.py
@@ -0,0 +1,58 @@
+def student():
+    def student_details():
+        Name = input("Enter Name:")
+        Roll_Number = int(input("Enter Roll Number:"))
+        Class = int(input("Enter class:"))
+        School = input("Enter school Name:")
+        sub1 = int(input("Tamil:"))
+        sub2 = int(input("English:"))
+        sub3 = int(input("Math:"))
+        sub4 = int(input("Scince:"))
+        sub5 = int(input("S.S:"))
+        Total = sub1 + sub2 + sub3 + sub4 + sub5
+        Percent = (Total/500)*(100) 
+    
+    
+        Student.write("Name :"+str(Name))
+        Student.write("\nRoll Number : "+str(Roll_Number))
+        Student.write("\nClass : "+str(Class))
+        Student.write("\nSchool Name : "+str(School))
+    
+        Student.writelines("Tamil="+str(sub1)+"\n")
+        Student.writelines("English="+str(sub2)+"\n")
+        Student.writelines("Math="+str(sub3)+"\n")
+        Student.writelines("Scince="+str(sub4)+"\n")
+        Student.writelines("S.S="+str(sub5)+"\n")
+        Student.writelines("Total="+str(Total)+"\n")
+        Student.write("Percentage="+str(Percent)+"%"+"\n")
+        if sub1 < 35:
+            Student.write("Fail")
+    
+        elif sub2 < 35:
+            Student.write("Fail")
+
+        elif sub3 < 35:
+            Student.write("Fail")
+
+        elif sub4 < 35:
+            Student.write("Fail")
+        
+        elif sub5 < 35:
+            Student.write("Fail")
+    
+        else:
+            Student.write("Pass")
+            
+    Student = []
+    for i in range(1,4):
+        Student = open(str(i)+"STD.txt","w")
+    Student = open("1STD.txt","w")
+    student_details()
+    Student = open("2STD.txt","w")
+    student_details()
+
+  
+   
+    
+   
+student()
